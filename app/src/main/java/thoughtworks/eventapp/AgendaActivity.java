a5a1e3bdf1c6886792b7c286a41e7e8a821c9e79
NazneenRupawalla <nazneenr@thoughtworks.com>
2016-05-04 19:45:37 +0530

    Vidhi/Nazneen - Initial commit.

---
+++ app/src/main/java/thoughtworks/eventapp/AgendaActivity.java
@@ -0,0 +1,13 @@
+package thoughtworks.eventapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AgendaActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_agenda);
+  }
+}
