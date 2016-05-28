742f37f0df56f3d83d0ad3512d5c3ef3ea2a1332
NazneenRupawalla <nazneenr@thoughtworks.com>
2016-05-28 16:26:50 +0530

    Improving the logic of date formatting.

--- app/src/main/java/thoughtworks/eventapp/viewmodel/SessionViewModel.java
+++ app/src/main/java/thoughtworks/eventapp/viewmodel/SessionViewModel.java
@@ -3,10 +3,8 @@ package thoughtworks.eventapp.viewmodel;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-import org.joda.time.DateTime;
-import org.joda.time.Interval;
-
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 import thoughtworks.eventapp.model.Session;
@@ -35,22 +33,13 @@ public class SessionViewModel implements Parcelable {
   };
 
   public String getDisplayTime() {
-    long duration = session.getEndTime().getTime() - session.getStartTime().getTime();
-    long hrs = TimeUnit.MILLISECONDS.toHours(duration);
-    long minutes = TimeUnit.MILLISECONDS.toMinutes(duration);
-    String hms;
-    if (hrs == 0) {
-      hms = String.format("%02dmin", minutes - TimeUnit.HOURS.toMinutes(hrs));
-    } else {
-      if ((minutes - TimeUnit.HOURS.toMinutes(hrs)) == 0) {
-        hms = String.format("%1dh", hrs);
-      } else {
-        hms = String.format("%1dh %02dmin", hrs, minutes - TimeUnit.HOURS.toMinutes(hrs));
-      }
-    }
-
-    return new SimpleDateFormat("hh:mm aaa").format(session.getStartTime()) + " - " + new SimpleDateFormat("hh:mm aaa").format(session.getEndTime())
-        + " (" + hms + ")";
+    SimpleDateFormat dateFormat = new SimpleDateFormat("hh:mm aaa");
+    Date startTime = session.getStartTime();
+    Date endTime = session.getEndTime();
+    long durationInMinutes = TimeUnit.MILLISECONDS.toMinutes(endTime.getTime() - startTime.getTime());
+    String formattedDuration = formattedDuration(durationInMinutes);
+    return String.format("%s - %s (%s)", dateFormat.format(startTime), dateFormat.format(endTime),
+        formattedDuration);
   }
 
   public String getName() {
@@ -65,5 +54,13 @@ public class SessionViewModel implements Parcelable {
   @Override
   public void writeToParcel(Parcel parcel, int i) {
     parcel.writeParcelable(session, 0);
+  }
+
+  private String formattedDuration(long durationInMinutes){
+    long numberOfHours = durationInMinutes / 60;
+    long numberOfMinutes = durationInMinutes % 60;
+    String formattedHours = numberOfHours==0?"":numberOfHours+"h ";
+    String formattedMinutes = numberOfMinutes==0?"":numberOfMinutes+"min";
+    return (formattedHours + formattedMinutes).trim();
   }
 }
