c54eea195eb714817ea4ffdb4609092e746a7c14
Vidhi Thakrar <vidhitha@thoughtworks.com>
2016-06-05 22:03:01 +0530

    Added @NonNull annotation for getClassOfType() in APIClient.

--- app/src/main/java/thoughtworks/eventapp/apiclient/APIClientCallback.java
+++ app/src/main/java/thoughtworks/eventapp/apiclient/APIClientCallback.java
@@ -1,8 +1,12 @@
 package thoughtworks.eventapp.apiclient;
 
+import android.support.annotation.NonNull;
+
 public interface APIClientCallback<T> {
 
   void onSuccess(T success);
 
+  //TODO: See whether NonNull check can be identified in android lint.
+  @NonNull
   Class<T> getClassOfType();
 }
