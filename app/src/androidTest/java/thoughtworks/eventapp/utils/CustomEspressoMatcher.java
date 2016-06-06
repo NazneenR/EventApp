aa825e104dde114a84c11be583d75ef12711e1f7
Vidhi Thakrar <vidhitha@thoughtworks.com>
2016-06-06 23:10:16 +0530

    Added espresso test for view pager titles.

---
+++ app/src/androidTest/java/thoughtworks/eventapp/utils/CustomEspressoMatcher.java
@@ -0,0 +1,45 @@
+package thoughtworks.eventapp.utils;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+public class CustomEspressoMatcher {
+
+  public static Matcher<View> atPositionInViewGroup(final Matcher<View> parentMatcher, final int childPosition) {
+    return new TypeSafeMatcher<View>() {
+      @Override
+      public void describeTo(Description description) {
+        description.appendText("with " + childPosition + " child view of type parentMatcher");
+      }
+
+      @Override
+      public boolean matchesSafely(View view) {
+        if (!(view.getParent() instanceof ViewGroup)) {
+          return parentMatcher.matches(view.getParent());
+        }
+
+        ViewGroup group = (ViewGroup) view.getParent();
+        return parentMatcher.matches(view.getParent()) && group.getChildAt(childPosition).equals(view);
+      }
+    };
+  }
+
+  public static Matcher<View> withNumberOfChildren(final int noOfChildren) {
+    return new TypeSafeMatcher<View>() {
+      @Override
+      public void describeTo(Description description) {
+        description.appendText("With number of children: " + noOfChildren);
+      }
+
+      @Override
+      protected boolean matchesSafely(View item) {
+        return item instanceof  ViewGroup && ((ViewGroup)item).getChildCount() == noOfChildren;
+      }
+    };
+  }
+
+}
--- app/src/androidTest/java/thoughtworks/eventapp/view/AgendaActivityTest.java
+++ app/src/androidTest/java/thoughtworks/eventapp/view/AgendaActivityTest.java
@@ -3,7 +3,9 @@ package thoughtworks.eventapp.view;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
 
+import org.hamcrest.Matcher;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -28,9 +30,12 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.swipeLeft;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.RootMatchers.isDialog;
+import static android.support.test.espresso.matcher.ViewMatchers.withChild;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static junit.framework.Assert.assertTrue;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.doAnswer;
@@ -39,6 +44,8 @@ import static org.mockito.Mockito.reset;
 import static thoughtworks.eventapp.model.Category.ASPIRE;
 import static thoughtworks.eventapp.model.Category.BELONG;
 import static thoughtworks.eventapp.model.Category.CREATE;
+import static thoughtworks.eventapp.utils.CustomEspressoMatcher.atPositionInViewGroup;
+import static thoughtworks.eventapp.utils.CustomEspressoMatcher.withNumberOfChildren;
 import static thoughtworks.eventapp.utils.DateUtil.getDate;
 
 @RunWith(AndroidJUnit4.class)
@@ -58,26 +65,22 @@ public class AgendaActivityTest {
   }
 
   @Test
+  public void shouldShowCategoriesAsViewPagersTitle(){
+    mockAPIClientForSuccess();
+
+    activityTestRule.launchActivity(new Intent());
+
+    Matcher<View> slidingTabStripViewMatcher = withClassName(is("android.support.design.widget.TabLayout$SlidingTabStrip"));
+    onView(slidingTabStripViewMatcher).check(matches(withNumberOfChildren(3)));
+    onView(atPositionInViewGroup(slidingTabStripViewMatcher, 0)).check(matches(withChild(withText("CREATE"))));
+    onView(atPositionInViewGroup(slidingTabStripViewMatcher, 1)).check(matches(withChild(withText("ASPIRE"))));
+    onView(atPositionInViewGroup(slidingTabStripViewMatcher, 2)).check(matches(withChild(withText("BELONG"))));
+
+  }
+
+  @Test
   public void shouldSwipeTab() throws InterruptedException {
-    doAnswer(new Answer() {
-      @Override
-      public Object answer(InvocationOnMock invocation) throws Throwable {
-        final APIClientCallback callback = (APIClientCallback) invocation.getArguments()[1];
-
-        Conference conference = new Conference();
-        Session session1 = new Session("Craft", "Try your hand at craft", getDate("2016-05-23T19:15:00+05:30"), getDate("2016-05-23T20:15:00+05:30"), ASPIRE, "Ballroom");
-        Session session2 = new Session("Craft", "Try your hand at craft", getDate("2016-05-23T19:15:00+05:30"), getDate("2016-05-23T20:15:00+05:30"), BELONG, "Ballroom");
-        Session session3 = new Session("Keynote", "By Roy Singham", getDate("2016-05-24T17:15:00+05:30"), getDate("2016-05-24T18:15:00+05:30"), CREATE, "Pre Function Area");
-        List<Session> sessionList = new ArrayList<>();
-        sessionList.add(session1);
-        sessionList.add(session2);
-        sessionList.add(session3);
-        conference.setSessions(sessionList);
-
-        callback.onSuccess(conference);
-        return null;
-      }
-    }).when(apiClient).get(eq(URL), any(APIClientCallback.class));
+    mockAPIClientForSuccess();
 
     activityTestRule.launchActivity(new Intent());
 
@@ -101,6 +104,28 @@ public class AgendaActivityTest {
     onView(withId(android.R.id.button1)).inRoot(isDialog()).check(matches(withText("OK")));
     onView(withId(android.R.id.button1)).inRoot(isDialog()).perform(click());
     assertTrue(activityTestRule.getActivity().isFinishing());
+  }
+
+  private void mockAPIClientForSuccess() {
+    doAnswer(new Answer() {
+      @Override
+      public Object answer(InvocationOnMock invocation) throws Throwable {
+        final APIClientCallback callback = (APIClientCallback) invocation.getArguments()[1];
+
+        Conference conference = new Conference();
+        Session session1 = new Session("Craft", "Try your hand at craft", getDate("2016-05-23T19:15:00+05:30"), getDate("2016-05-23T20:15:00+05:30"), ASPIRE, "Ballroom");
+        Session session2 = new Session("Craft", "Try your hand at craft", getDate("2016-05-23T19:15:00+05:30"), getDate("2016-05-23T20:15:00+05:30"), BELONG, "Ballroom");
+        Session session3 = new Session("Keynote", "By Roy Singham", getDate("2016-05-24T17:15:00+05:30"), getDate("2016-05-24T18:15:00+05:30"), CREATE, "Pre Function Area");
+        List<Session> sessionList = new ArrayList<>();
+        sessionList.add(session1);
+        sessionList.add(session2);
+        sessionList.add(session3);
+        conference.setSessions(sessionList);
+
+        callback.onSuccess(conference);
+        return null;
+      }
+    }).when(apiClient).get(eq(URL), any(APIClientCallback.class));
   }
 
   @NonNull
