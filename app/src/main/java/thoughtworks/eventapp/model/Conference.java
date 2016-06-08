88c1f5cda638a97e01fe7a88d9ac34027c5cb0ea
NazneenRupawalla <nazneenr@thoughtworks.com>
2016-06-08 12:15:37 +0530

    Need to relook at this addition of setter. Parsing fails without that.

--- app/src/main/java/thoughtworks/eventapp/model/Conference.java
+++ app/src/main/java/thoughtworks/eventapp/model/Conference.java
@@ -6,7 +6,13 @@ import java.util.List;
 public class Conference {
   private List<Session> sessions;
 
+  public Conference(){  }
+
   public Conference(List<Session> sessions){
+    this.sessions = sessions;
+  }
+
+  public void setSessions(List<Session> sessions){
     this.sessions = sessions;
   }
 
